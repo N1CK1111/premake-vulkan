9c72912532aa2e2bd8da389a11be1a6e7e72a51c
N1CK1111 <160319015+N1CK1111@users.noreply.github.com>
2024-09-08 20:17:27 +1000

    Fixed issue in vk_initializers

--- App/Source/vk_initializers.h
+++ App/Source/vk_initializers.h
@@ -8,11 +8,11 @@ namespace vkinit {
 	VkCommandBufferAllocateInfo command_buffer_allocate_info(VkCommandPool pool, uint32_t count = 1, VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY);
 
     // Shader pipeline stuff
-    VkPipelineShaderStageCreateInfo vkinit::pipeline_shader_stage_create_info(VkShaderStageFlagBits stage, VkShaderModule shaderModule);
-    VkPipelineVertexInputStateCreateInfo vkinit::vertex_input_state_create_info();
-    VkPipelineInputAssemblyStateCreateInfo vkinit::input_assembly_create_info(VkPrimitiveTopology topology);
-    VkPipelineRasterizationStateCreateInfo vkinit::rasterization_state_create_info(VkPolygonMode polygonMode);
-    VkPipelineMultisampleStateCreateInfo vkinit::multisampling_state_create_info();
-    VkPipelineColorBlendAttachmentState vkinit::color_blend_attachment_state();
-    VkPipelineLayoutCreateInfo vkinit::pipeline_layout_create_info();
+    VkPipelineShaderStageCreateInfo pipeline_shader_stage_create_info(VkShaderStageFlagBits stage, VkShaderModule shaderModule);
+    VkPipelineVertexInputStateCreateInfo vertex_input_state_create_info();
+    VkPipelineInputAssemblyStateCreateInfo input_assembly_create_info(VkPrimitiveTopology topology);
+    VkPipelineRasterizationStateCreateInfo rasterization_state_create_info(VkPolygonMode polygonMode);
+    VkPipelineMultisampleStateCreateInfo multisampling_state_create_info();
+    VkPipelineColorBlendAttachmentState color_blend_attachment_state();
+    VkPipelineLayoutCreateInfo pipeline_layout_create_info();
 }
