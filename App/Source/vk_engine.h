6d99c24db225f6ca1aa2e080a9ce09f47b9dba3d
N1CK1111 <mgill27@duck.com>
2024-09-09 13:19:05 +1000

    Changed window size

--- App/Source/vk_engine.h
+++ App/Source/vk_engine.h
@@ -9,7 +9,7 @@ public:
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 
-	VkExtent2D _windowExtent{ 1700 , 900 };
+	VkExtent2D _windowExtent{ 600 , 400 };
 
     struct SDL_Window* _window{ nullptr };
 	
